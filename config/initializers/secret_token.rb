5a94592daf6606abde5b8c89b71c0daa30c8714c
James Klein <kleinjm007@gmail.com>
2015-07-28 11:16:37 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppleThief::Application.config.secret_token = '2ef566a3d8588ed7a38b4e0d78ce59081efd8dbe8110e1ae02bf1a00fabc03cbb5dc25cc098b97ae96cd0bf69c4a8adb79a29f63b2feb1af9040ff00b506e672'
